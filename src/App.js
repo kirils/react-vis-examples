1cb6812553d72386f70adb6a9eb4f33f6b929248
Kiril Stoilov <kiril.stoilov@gmail.com>
2019-01-26 22:02:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import BarChart from './BarChart'
+import AxisOn0 from './axis-on';
+import HorizontalBarChart from './HorizontalBarChart';
+import LinemarkChart from './linemark-chart';
+import SimpleRadialChart from './donut-chart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Uber Visualization Charts - react-vis examples</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +27,11 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <BarChart />
+        <HorizontalBarChart />
+        <AxisOn0 />
+        <LinemarkChart />
+        <SimpleRadialChart />
       </div>
     );
   }
